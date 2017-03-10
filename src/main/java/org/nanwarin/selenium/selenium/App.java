a579bf8f6c8a944e009c658422bb9d2c2966d106
nan2iz <nan2iz.ch@gmail.com>
2017-03-09 16:43:58 -0800

    add chrome driver

--- src/main/java/org/nanwarin/selenium/selenium/App.java
+++ src/main/java/org/nanwarin/selenium/selenium/App.java
@@ -1,38 +1,61 @@
 package org.nanwarin.selenium.selenium;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /***
- * Example code from : http://www.guru99.com/first-webdriver-script.html
  * @author nchantarutai
  *
  */
 
 public class App 
 {
+	
+	static String baseUrl = "http://bronco-planner.com/";
+	static String expectedTitle = "Bronco-Planner - CPP Student Organizer";
+	
     public static void main( String[] args )
     {
+    	firefoxBrowser();
+    	chromeBrowser();
+    }
+    
+    public static void firefoxBrowser(){
     	//This is for window, set up the environment based on driver path
     	//geckordriver is firefox driver
     	System.setProperty("webdriver.gecko.driver","C:\\lib\\geckodriver.exe");
-    	WebDriver driver = new FirefoxDriver();
-    	String baseUrl = "http://bronco-planner.com/";
-    	String expectedTitle = "Bronco-Planner - CPP Student Organizer";
-    	String actualTitle = "";
+    	
+    	WebDriver driverFirefox = new FirefoxDriver();
     	
     	//Launch Firefox and direct it to the Base URL
-    	driver.get(baseUrl);
-    	
-    	actualTitle = driver.getTitle();
-    	
-    	if(actualTitle.contentEquals(expectedTitle))
-    		System.out.println("PASSED");
+    	driverFirefox.get(baseUrl);
+
+    	String result = "";
+    	//Easy test, checking title
+    	if(driverFirefox.getTitle().contentEquals(expectedTitle))
+    		result = "PASSED";
     	else
-    		System.out.println("FAILED");
+    		result = "FAILED";
     	
-    	driver.close();
+    	System.out.println("FIREFOX -- " + result);
+    	driverFirefox.close();
     	
-    	System.exit(0);
+    	//Need to check, for some reason firefox driver didn't automatic close the browser
+
+    }
+    
+    public static void chromeBrowser(){
+    	System.setProperty("webdriver.chrome.driver", "C:\\lib\\chromedriver.exe");
+    	WebDriver driverChrome = new ChromeDriver();
+    	
+    	driverChrome.get(baseUrl);
+    	if(driverChrome.getTitle().contentEquals(expectedTitle))
+    		System.out.println("CHROME -- PASSED");
+    	else
+    		System.out.println("CHROME -- FAILED");
+    	
+    	driverChrome.close();
+    	
     }
 }
